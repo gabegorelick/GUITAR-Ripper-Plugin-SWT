19cec170bc263c18cf9834da5472f3f3a21aa746
Gabe Gorelick <gabegorelick@gmail.com>
2011-02-19 00:53:05 -0500

    Add JUnit and some example tests.

---
+++ test/edu/umd/cs/guitar/ripper/test/SWTRipperTest.java
@@ -0,0 +1,16 @@
+package edu.umd.cs.guitar.ripper.test;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import edu.umd.cs.guitar.ripper.SWTRipper;
+
+public class SWTRipperTest {
+
+	@Test
+	public void testSWTRipper() {
+		SWTRipper ripper = new SWTRipper(null);
+		assertNotNull(ripper.getMonitor());
+	}
+}
