e842e453584a01943fc9cac75e433f9af26519c0
Gabe Gorelick <gabegorelick@gmail.com>
2011-03-24 00:15:59 -0400

    Add SWTRipperRunner to simplify executing ripper and starting application.

---
+++ src/edu/umd/cs/guitar/ripper/SWTRipperRunner.java
@@ -0,0 +1,55 @@
+package edu.umd.cs.guitar.ripper;
+
+import org.kohsuke.args4j.CmdLineException;
+
+/**
+ * Convenience class to run {@link SWTRipper}.
+ * 
+ * Handles starting the application under test and the execution of the ripper.
+ * 
+ * @author Gabe Gorelick
+ * 
+ */
+public class SWTRipperRunner {
+
+	private final SWTRipper ripper;
+	private final Thread ripperThread;
+
+	public SWTRipperRunner(final SWTRipper ripper) {
+		this.ripper = ripper;
+		this.ripperThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					ripper.execute();
+				} catch (CmdLineException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		// set ripper thread name to make debugging easier
+		ripperThread.setName("ripper-thread");
+	}
+
+	/**
+	 * Start the ripper and the application under test. This method must be
+	 * called on the main thread so that the application under test is started
+	 * on the main thread. The ripper is executed on its own thread.
+	 */
+	public void start() {
+		ripperThread.start();
+		SWTRipperMonitor monitor = (SWTRipperMonitor) ripper.getMonitor();
+
+		// start GUI on main thread, this blocks until GUI terminates
+		monitor.getApplication().startGUI();
+		try {
+			// prevent the main thread closing (and thus the JVM) before
+			// the ripper is done
+			ripperThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
